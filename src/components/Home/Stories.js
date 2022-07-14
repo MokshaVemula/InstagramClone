435621d8d4a4707ee4ed37b33568fb322ba033e8
MokshaVemula <moksha.vemula@xcubelabs.com>
2022-07-14 16:29:39 +0530

    first commit

--- src/components/Home/Stories.js
+++ src/components/Home/Stories.js
@@ -1,66 +1,80 @@
 import React from "react"
 import { Text, View, ScrollView,Image, StyleSheet,TouchableOpacity } from "react-native"
 import Icon from "react-native-vector-icons/Ionicons";
+import Status from "./Status";
+import { useNavigation } from "@react-navigation/native";
+
 const USERS = [{
     user: 'Moksha_Moni',
-    image: 'https://i.pinimg.com/550x/1f/83/c5/1f83c5ce9090c12d1969ad7a3745cc82.jpg'
+    image: 'https://i.pinimg.com/550x/1f/83/c5/1f83c5ce9090c12d1969ad7a3745cc82.jpg',
+    key:1,
 },
 {
     user: 'itsmemeghss',
-    image: 'https://image.shutterstock.com/image-photo/young-adult-profile-picture-red-260nw-1655747050.jpg'
+    image: 'https://image.shutterstock.com/image-photo/young-adult-profile-picture-red-260nw-1655747050.jpg',
+    key:2,
 },
 {
     user: 'jyoo__21',
-    image: 'https://image.shutterstock.com/image-photo/smiling-teen-girl-1617-year-260nw-1700600221.jpg'
+    image: 'https://image.shutterstock.com/image-photo/smiling-teen-girl-1617-year-260nw-1700600221.jpg',
+    key:3,
 },
 {
     user: 'pravalika_rita',
-    image: 'https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80'
+    image: 'https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+    key:4,
 },
 {
     user: 's_g_eswar',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmwlpMo2uIF90YQTSfEad28sj2ku7hd9zWpA&usqp=CAU'
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmwlpMo2uIF90YQTSfEad28sj2ku7hd9zWpA&usqp=CAU',
+    key:5,
 },
 {
     user: 'meghnaa_08',
-    image: 'https://www.statuspik.com/wp-content/uploads/2020/12/best-whatsapp-dp-for-girls-6.jpg'
+    image: 'https://www.statuspik.com/wp-content/uploads/2020/12/best-whatsapp-dp-for-girls-6.jpg',
+    key:7,
 },
 {
     user: 'jupudisrividya',
-    image: 'https://image.shutterstock.com/image-photo/young-adult-profile-picture-red-260nw-1655747050.jpg'
+    image: 'https://image.shutterstock.com/image-photo/young-adult-profile-picture-red-260nw-1655747050.jpg',
+    key:8,
 },
 {
     user: 'katam.niharika',
-    image: 'https://image.shutterstock.com/image-photo/smiling-teen-girl-1617-year-260nw-1700600221.jpg'
+    image: 'https://image.shutterstock.com/image-photo/smiling-teen-girl-1617-year-260nw-1700600221.jpg',
+    key:9,
 },
 {
     user: 'ananya_shati',
-    image: 'https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80'
+    image: 'https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+    key:10,
 },
 {
     user: 'uday_83',
-    image: 'https://www.statuspik.com/wp-content/uploads/2020/12/best-whatsapp-dp-for-girls-6.jpg'
+    image: 'https://www.statuspik.com/wp-content/uploads/2020/12/best-whatsapp-dp-for-girls-6.jpg',
+    key:11,
 },
 
 ]
 
 const Stories = () =>{
+    const Navigation = useNavigation()
     
     return(
         <View style={{marginBottom:13}}>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                <TouchableOpacity style={{alignItems:'center', justifyContent:'center'}}>
+                <TouchableOpacity style={{alignItems:'center', justifyContent:'center'}} onPress={()=>Navigation.push('Status')}>
                     <Image source={{uri:'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'}} style={styles.story}/>
                     <View style={styles.badge}>
                         <Icon name="add-outline" size={20} color='white'/>
                     </View>
                     <Text style={{color:'white'}}>Your story</Text>
-                </TouchableOpacity>
+                </TouchableOpacity >
                 {USERS.map((story, index)=>(
-                    <View key={index} style={{alignItems:'center', justifyContent:'center'}}>
+                    <TouchableOpacity key={index} style={{alignItems:'center', justifyContent:'center'}} onPress={()=>Navigation.push('Status',{name:story.image, name:story.user})}>
                         <Image source={{uri:story.image}} style={styles.story}/>
                         <Text style={{color:'white',marginLeft:5}}>{story.user.length>9 ? story.user.slice(0,10).toLowerCase() + '...' : story.user.toLowerCase()}</Text>
-                    </View>
+                    </TouchableOpacity>
                 ))}
             </ScrollView>
 
--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -15,6 +15,7 @@ import Register from "../Authentication/Register";
 import EditProfile from "../components/Profile/EditProfile";
 import FriendProfile from "../components/notification/FriendProfile";
 import UserPosts from "../components/Profile/UserPosts";
+import Status from "../components/Home/Status";
 
 const Tab = createBottomTabNavigator()
 
@@ -32,6 +33,7 @@ const BottomTabNavigation = () =>{
                         backgroundColor: 'black',
                         height: 1.5,
                       },
+                      
                 })}
             >
                 <Tab.Screen name="Home" component={Home} options={{tabBarIcon:()=>(<Icon name='home-outline' color='white' size={30}/>)}} />
@@ -69,6 +71,7 @@ const HomeStack = () =>{
                 <Stack.Screen name='UserPosts' component={UserPosts} />
                 <Stack.Screen name="EditProfile" component={EditProfile} />
                 <Stack.Screen name="FriendProfile" component={FriendProfile} />
+                <Stack.Screen name="Status" component={Status} />
                 
             </Stack.Navigator>
         </NavigationContainer>
